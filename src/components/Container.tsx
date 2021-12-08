d7e7ccb0ba845f70573aaa454be4ca80341a6197
Austin Stowe <akstowe.95@gmail.com>
2021-12-08 08:41:36 -0800

    Refactor styling for navbar

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,34 +1,11 @@
-import routes from 'constants/routes'
 import { FC } from 'react'
 import { Router, ReactLocation, Outlet, Route } from 'react-location'
 import { ReactLocationDevtools } from 'react-location-devtools'
-import Education from './Education/Education'
-import Footer from './Footer/Footer'
 import Header from './Header/Header'
-import HomePage from './HomePage/HomePage'
-import Personal from './Personal/Personal'
-import Professional from './Professional/Professional'
 
 const location = new ReactLocation()
 
-const reactLocationRoutes: Route[] = [
-    {
-        path: routes.HOME,
-        element: <HomePage />,
-    },
-    {
-        path: routes.PERSONAL,
-        element: <Personal />,
-    },
-    {
-        path: routes.PROFESSIONAL,
-        element: <Professional />,
-    },
-    {
-        path: routes.EDUCATION,
-        element: <Education />,
-    },
-]
+const reactLocationRoutes: Route[] = []
 
 const Container: FC = () => {
     return (
@@ -37,7 +14,6 @@ const Container: FC = () => {
                 <Header />
                 <Outlet />
                 <ReactLocationDevtools initialIsOpen={false} />
-                <Footer />
             </Router>
         </div>
     )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,27 @@
 import { FC } from 'react'
-import SiteName from 'components/SiteName/SiteName'
-import Navbar from 'components/Navbar/Navbar'
+import { Link } from 'react-location'
+
+import routes from 'constants/routes'
+import './Header.css'
 
 const Header: FC = () => {
     return (
-        <>
-            <SiteName />
-            <Navbar />
-        </>
+        <nav>
+            <h1 className="site-title">Austin Stowe</h1>
+            <ul className="nav-list">
+                {Object.keys(routes).map((key, index) => (
+                    <li key={`${key}Link`} className="nav-list-item">
+                        <Link 
+                            className="link" 
+                            to={routes[key as keyof typeof routes]}
+                            style={{animationDelay: `${((index+1) * 500)+2250}ms`}}
+                        >
+                            {key.charAt(0) + key.slice(1, key.length).toLowerCase()}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
 
